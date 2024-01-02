fa1aeb7a4a04fc2f4c67d5015aa62e3ad73c4d56
Nick Monteleone <nicholasmonteleone25@gmail.com>
2024-01-02 12:10:17 -0800

    step 1 completed

---
+++ step1.js
@@ -0,0 +1,21 @@
+"use strict";
+
+const fsP = require('fs/promises');
+
+const argv = process.argv;
+
+/** Read file for inputted file path str and log contents of file
+ * Print error if given path of non-existant file */
+
+async function cat(filePath) {
+
+  try {
+    const contents = await fsP.readFile(filePath, 'utf8');
+    console.log(contents);
+  } catch (err) {
+    console.error('Error:', err.message);
+    process.exit(1);
+  }
+}
+
+cat(argv[2]);
